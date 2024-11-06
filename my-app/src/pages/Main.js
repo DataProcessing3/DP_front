fc57fc3a22effaf22d6aaf1d10671e93d4e176b3
leeseunming <is3500294@gmail.com>
2024-11-06 18:40:23 +0900

    사진 클릭시 각 페이지로 이동하는 Nav 설정
    흰 상자 클릭시 각각의 사이트 (추천,뉴스,정보,등등)으로 이동하게끔 추가 import 선언 및 코드 변경

--- my-app/src/pages/Main.js
+++ my-app/src/pages/Main.js
@@ -1,44 +1,46 @@
-import React from 'react'; // React와 useState를 import
 
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import "../App.css";
 const Main = (props) => {
 
 
-    return (
+	return (
 		<>
-		<div className='body-form'>
-			<div className='sub-title'>
-				메뉴
+			<div className='body-form'>
+				<div className='sub-title'>
+					메뉴
+				</div>
+				<div className='data-container'>
+					<Link to="/DataAnalysis" className="rectangle" style={{ textDecoration: 'none' }}>
+						<img src="/img/analy.png" alt="Your Image" style={{ maxWidth: '40%', maxHeight: '40%', objectFit: 'contain' }} />
+						<p>분석</p>
+					</Link>
+					<Link to="/Information" className="rectangle" style={{ textDecoration: 'none' }}>
+						<img src="/img/info.png" alt="Your Image" style={{ maxWidth: '40%', maxHeight: '40%', objectFit: 'contain' }} />
+						<p>정보</p>
+					</Link>
+				</div>
+				<div className='data-container'>
+					<Link to="/NaverNews" className="rectangle" style={{ textDecoration: 'none' }}>
+						<img src="/img/news.png" alt="Your Image" style={{ maxWidth: '40%', maxHeight: '40%', objectFit: 'contain' }} />
+						<p>뉴스</p>
+					</Link>
+					<Link to="/Recommand" className="rectangle" style={{ textDecoration: 'none' }}>
+						<img src="/img/rec.png" alt="Your Image" style={{ maxWidth: '40%', maxHeight: '40%', objectFit: 'contain' }} />
+						<p>추천</p>
+					</Link>
+				</div>
+				<div className='sub-title'>
+					중시 동향
+				</div>
+				<div className='data-container'>
+					<div className="rectangle"></div>
+					<div className="rectangle"></div>
+				</div>
 			</div>
-			<div className='data-container'>
-				<div className="rectangle">
-					<img src="/img/analy.png" alt="Your Image" style={{ maxWidth: '40%', maxHeight: '40%', objectFit: 'contain' }} />
-					<p>분석</p>
-					</div>
-				<div className="rectangle">
-					<img src="/img/info.png" alt="Your Image" style={{ maxWidth: '40%', maxHeight: '40%', objectFit: 'contain' }} />
-					<p>정보</p>
-					</div>
-			</div>
-			<div className='data-container'>
-				<div className="rectangle">
-					<img src="/img/news.png" alt="Your Image" style={{ maxWidth: '40%', maxHeight: '40%', objectFit: 'contain' }} />
-					<p>뉴스</p>
-					</div>
-				<div className="rectangle">
-					<img src="/img/rec.png" alt="Your Image" style={{ maxWidth: '40%', maxHeight: '40%', objectFit: 'contain' }} />
-					<p>추천</p>
-					</div>
-			</div>
-			<div className='sub-title'>
-				중시 동향
-			</div>
-			<div className='data-container'>
-				<div className="rectangle"></div>
-				<div className="rectangle"></div>
-			</div>
-		</div>
 		</>
-    );
+	);
 };
 
 export default Main;
